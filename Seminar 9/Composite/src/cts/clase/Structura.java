abc64a3eaf2b67e6c84987e0dde61e5ab92a76ff
Iuli245 <rijnoveanu_iulian@yahoo.com>
2025-05-12 16:31:33 +0300

    git add

---
+++ Seminar 9/Composite/src/cts/clase/Structura.java
@@ -0,0 +1,8 @@
+package cts.clase;
+
+public interface Structura {
+    void adaugaStructura(Structura structura) throws Exception;
+    void stergeStructura(Structura structura) throws Exception;
+    void afiseazaDescriere();
+    Structura getStructura(int index) throws Exception;
+}
